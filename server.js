8155e0b236bdd1a81562c4f8f78e04a7fc21b967
Chris Bassano <christo4b@gmail.com>
2016-12-17 18:26:11 -0800

    express server and simple 200 server test

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 7777
+
+app.get('/', function(req, res){
+  res.send("Hello HL7")
+})
+
+const server = app.listen(port, ()=> {
+  console.log("Server running on port:", port)
+})
+
+
+module.exports = { 
+  app: app,
+  closeServer: function(){
+    server.close()
+  }
+}
--- test/test.js
+++ test/test.js
@@ -1,5 +1,8 @@
 const assert = require('assert')
+const request = require('request')
+const { app, closeServer } = require('../server.js')
 
+console.log(app)
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
@@ -7,3 +10,19 @@ describe('Array', function() {
     });
   });
 });
+
+describe("Server Running", function(){
+  describe("GET /", function(){
+    it("returns status code 200", function(done){
+      request.get("http://localhost:7777/", function(err, res, body){
+        console.log(err)
+        assert.equal(200, res.statusCode)
+        closeServer()
+        done()
+      })
+    })    
+  })
+})
+
+
+
